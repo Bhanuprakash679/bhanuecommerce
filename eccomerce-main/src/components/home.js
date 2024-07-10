4cebd1c9beae0f0434e311c68747cef28947fab9
Bhanuprakash679 <152599814+Bhanuprakash679@users.noreply.github.com>
2024-07-10 11:20:18 +0530

    Initial commit

---
+++ eccomerce-main/src/components/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home.css'
+const Home = () => {
+  return (
+    <div className="container">
+      <h2>Welcome to our Bhanuprakash Website</h2>
+      <p>Explore our products and services.</p>
+
+      <p className='foot'>ALL CREDITS ARE RESERVED TO @ 22071A0527</p>
+    </div>
+  );
+};
+
+export default Home;
